f85195e73d85f2faba2c74a2efe4a7bd5b0e3c28
Berkay9712 <s_olmaz21@stud.hwr-berlin.de>
2025-02-10 23:16:24 +0100

    Anpassung app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///surveys.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 class Survey(db.Model):
@@ -17,14 +18,14 @@ class Question(db.Model):
     survey_id = db.Column(db.Integer, db.ForeignKey('survey.id'), nullable=False)
 
 class Response(db.Model):
-    __tablename__ = 'response'
     id = db.Column(db.Integer, primary_key=True)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
     answer = db.Column(db.String, nullable=False)
-
     question = db.relationship('Question', backref=db.backref('responses', lazy=True))
 
-db.create_all()
+# Datenbanktabellen erstellen (innerhalb eines Anwendungskontexts)
+with app.app_context():
+    db.create_all()
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
@@ -32,19 +33,16 @@ def create():
         title = request.form['title']
         questions_text = request.form.getlist('questions[]')
 
-        # Neue Umfrage speichern
         new_survey = Survey(title=title)
         db.session.add(new_survey)
         db.session.commit()
 
-        # Fragen speichern
         for question_text in questions_text:
             question = Question(text=question_text, survey_id=new_survey.id)
             db.session.add(question)
-
+        
         db.session.commit()
 
-        # Link zur Umfrage generieren
         survey_link = url_for('survey', survey_id=new_survey.id, _external=True)
         return render_template('created.html', survey_link=survey_link)
 
@@ -74,3 +72,4 @@ def results(survey_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
